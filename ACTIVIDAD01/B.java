5eb7fa153bdae10ec4b694732ba207012cd6a493
Diego <Diego@gmail.com>
2024-02-23 16:29:11 -0500

    Añadida la clase B en la rama feature-2

---
+++ ACTIVIDAD01/B.java
@@ -0,0 +1,16 @@
+public class B {
+    private int number;
+
+    public B(int number) {
+        this.number = number;
+    }
+
+    // Getter y setter para number (opcional)
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
