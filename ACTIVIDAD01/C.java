91f8b4f65e487a94ac919f76880c1f5df7ba8f59
Diego <Diego@gmail.com>
2024-02-23 16:30:42 -0500

    Añadida la clase C en la rama feature-3

---
+++ ACTIVIDAD01/C.java
@@ -0,0 +1,16 @@
+public class C {
+    private String name;
+
+    public C(String name) {
+        this.name = name;
+    }
+
+    // Getter y setter para name (opcional)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
