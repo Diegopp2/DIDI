1fc7e97a990690992b321dcfa2558b917c3fc308
Diego <Diego@gmail.com>
2024-02-23 16:18:42 -0500

    Añadida la clase A con los atributos foo y bar en la rama feature-1

---
+++ ACTIVIDAD01/A.java
@@ -0,0 +1,26 @@
+public class A {
+    private String foo;
+    private String bar;
+
+    public A(String foo, String bar) {
+        this.foo = foo;
+        this.bar = bar;
+    }
+
+    // Getters y setters para foo y bar (opcional)
+    public String getFoo() {
+        return foo;
+    }
+
+    public void setFoo(String foo) {
+        this.foo = foo;
+    }
+
+    public String getBar() {
+        return bar;
+    }
+
+    public void setBar(String bar) {
+        this.bar = bar;
+    }
+}
